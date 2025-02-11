659ab9462c5a6b5dbf1836dd0dd8ebaeb93e0825
Tirth Patel <tirth.acct+github@gmail.com>
2025-02-11 18:04:26 -0500

    chore: remove all-in button

--- javascript/game.js
+++ javascript/game.js
@@ -674,7 +674,7 @@ function updateCallButton(value) {
     document.getElementById("call-button").setAttribute('value', value)
 }
 
-const items = ["$10", "$20", "$30", "$40", "$50", "$60", "$70", "$80", "$90", "$100", "$110", "$120", "$130", "$140", "$150", "$160", "$170", "$180", "$190", "ALL-IN"];
+const items = ["$10", "$20", "$30", "$40", "$50", "$60", "$70", "$80", "$90", "$100", "$110", "$120", "$130", "$140", "$150", "$160", "$170", "$180", "$190", "$200"/* "ALL-IN" */];
 createWheel(items);
 
 // Apply the momentus function
